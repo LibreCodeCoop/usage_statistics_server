657fd16869492cff8532f3b25fca8682e03a07f6
Vitor Mattos <vitor@php.rio>
2026-09-07 06:56:01 -0300

    feat: add Nextcloud application bootstrap
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ lib/AppInfo/Application.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\UsageStatisticsServer\AppInfo;
+
+use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
+
+final class Application extends App implements IBootstrap {
+    public const APP_ID = 'usage_statistics_server';
+
+    public function __construct() {
+        parent::__construct(self::APP_ID);
+    }
+
+    public function register(IRegistrationContext $context): void {
+    }
+
+    public function boot(IBootContext $context): void {
+    }
+}
