d0164ec10cfbc9409fb223b057e45c568562f883
Vitor Mattos <vitor@php.rio>
2026-09-07 07:21:02 -0300

    test: add Nextcloud PHPUnit bootstrap
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ tests/php/bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * SPDX-FileCopyrightText: 2026 Vitor Mattos
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+declare(strict_types=1);
+
+use OCP\App\IAppManager;
+use OCP\Server;
+
+if (!defined('PHPUNIT_RUN')) {
+    define('PHPUNIT_RUN', 1);
+}
+
+require_once __DIR__ . '/../../../../lib/base.php';
+require_once __DIR__ . '/../../../../tests/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+Server::get(IAppManager::class)->loadApp('usage_statistics_server');
