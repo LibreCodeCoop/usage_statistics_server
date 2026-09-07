e25ce048cc911e0019eecfb92a10d80ca68faa53
Vitor Mattos <vitor@php.rio>
2026-09-07 13:21:16 -0300

    refactor: register settings routes with ApiRoute
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- lib/Controller/SettingsController.php
+++ lib/Controller/SettingsController.php
@@ -6,10 +6,13 @@ namespace OCA\UsageStatisticsServer\Controller;
 
 use OCA\UsageStatisticsServer\Service\SettingsService;
 use OCP\AppFramework\Http;
+use OCP\AppFramework\Http\Attribute\ApiRoute;
+use OCP\AppFramework\Http\Attribute\OpenAPI;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\AppFramework\OCSController;
 use OCP\IRequest;
 
+#[OpenAPI(scope: OpenAPI::SCOPE_ADMINISTRATION, tags: ['settings'])]
 final class SettingsController extends OCSController {
     public function __construct(
         string $appName,
@@ -19,6 +22,12 @@ final class SettingsController extends OCSController {
         parent::__construct($appName, $request);
     }
 
+    /**
+     * Get usage statistics server settings
+     *
+     * @return DataResponse<Http::STATUS_OK, array{retentionDays:int,minimumRetentionDays:int,maximumRetentionDays:int}, array{}>
+     */
+    #[ApiRoute(verb: 'GET', url: '/api/{apiVersion}/admin/settings', requirements: ['apiVersion' => '(v1)'])]
     public function get(): DataResponse {
         return new DataResponse([
             'retentionDays' => $this->settings->getRetentionDays(),
@@ -27,6 +36,14 @@ final class SettingsController extends OCSController {
         ]);
     }
 
+    /**
+     * Update usage statistics server settings
+     *
+     * @param int $retentionDays Number of days to retain reports
+     *
+     * @return DataResponse<Http::STATUS_OK, array{retentionDays:int}, array{}>|DataResponse<Http::STATUS_BAD_REQUEST, array{error:string,message:string}, array{}>
+     */
+    #[ApiRoute(verb: 'PUT', url: '/api/{apiVersion}/admin/settings', requirements: ['apiVersion' => '(v1)'])]
     public function update(int $retentionDays): DataResponse {
         try {
             $retentionDays = $this->settings->setRetentionDays($retentionDays);
