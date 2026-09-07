80a90684ff674be96b1c6318ee6384862baf3974
Vitor Mattos <vitor@php.rio>
2026-09-07 15:53:21 -0300

    test: add Behat Nextcloud context
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ tests/integration/features/bootstrap/FeatureContext.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2026 Vitor Mattos
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+use Behat\Hook\BeforeSuite;
+use Behat\Testwork\Hook\Scope\BeforeSuiteScope;
+use Libresign\NextcloudBehat\NextcloudApiContext;
+
+final class FeatureContext extends NextcloudApiContext {
+    #[BeforeSuite]
+    public static function beforeSuite(BeforeSuiteScope $scope): void {
+        parent::beforeSuite($scope);
+        self::runCommand('config:system:set debug --value true --type boolean');
+        self::runCommand('config:system:set auth.bruteforce.protection.enabled --value false --type boolean');
+        self::runCommand('config:system:set ratelimit.protection.enabled --value false --type boolean');
+        self::runCommand('app:enable --force usage_statistics_server');
+    }
+}
