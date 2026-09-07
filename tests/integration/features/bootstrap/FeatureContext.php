21e17bcdf115f8ff4af4a2321a559aceff25743f
Vitor Mattos <vitor@php.rio>
2026-09-07 16:02:16 -0300

    test: support anonymous Behat requests
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/integration/features/bootstrap/FeatureContext.php
+++ tests/integration/features/bootstrap/FeatureContext.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
  */
 
 use Behat\Hook\BeforeSuite;
+use Behat\Step\Given;
 use Behat\Testwork\Hook\Scope\BeforeSuiteScope;
 use Libresign\NextcloudBehat\NextcloudApiContext;
 
@@ -19,5 +20,10 @@ final class FeatureContext extends NextcloudApiContext {
         self::runCommand('config:system:set auth.bruteforce.protection.enabled --value false --type boolean');
         self::runCommand('config:system:set ratelimit.protection.enabled --value false --type boolean');
         self::runCommand('app:enable --force usage_statistics_server');
+    }
+
+    #[Given('as anonymous user')]
+    public function asAnonymousUser(): void {
+        $this->setCurrentUser('');
     }
 }
