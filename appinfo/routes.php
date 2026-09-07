56b2399cef1fcb99f551a66df1ee1466d6e381f5
Vitor Mattos <vitor@php.rio>
2026-09-07 10:24:26 -0300

    feat: add server settings routes
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -7,6 +7,8 @@ return [
         ['name' => 'report#create', 'url' => '/api/v1/reports', 'verb' => 'POST'],
         ['name' => 'schema#create', 'url' => '/api/v1/admin/schemas', 'verb' => 'POST'],
         ['name' => 'schema#get', 'url' => '/api/v1/admin/schemas/{application}/{schemaVersion}', 'verb' => 'GET'],
+        ['name' => 'settings#get', 'url' => '/api/v1/admin/settings', 'verb' => 'GET'],
+        ['name' => 'settings#update', 'url' => '/api/v1/admin/settings', 'verb' => 'PUT'],
         ['name' => 'statistics#summary', 'url' => '/api/v1/admin/{application}', 'verb' => 'GET'],
         ['name' => 'statistics#distribution', 'url' => '/api/v1/admin/{application}/metrics/{category}/{key}/distribution', 'verb' => 'GET'],
         ['name' => 'statistics#numerical', 'url' => '/api/v1/admin/{application}/metrics/{category}/{key}/numerical', 'verb' => 'GET'],
