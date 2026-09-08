85b438385a8ed8cb9360f848fc07d5a629a8cfd4
Vitor Mattos <vitor@php.rio>
2026-09-07 21:57:05 -0300

    feat: add numerical statistics routes

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -7,6 +7,7 @@ return [
         ['name' => 'report#create', 'url' => '/api/v1/reports', 'verb' => 'POST'],
         ['name' => 'statistics#summary', 'url' => '/api/v1/admin/{application}', 'verb' => 'GET'],
         ['name' => 'statistics#distribution', 'url' => '/api/v1/admin/{application}/metrics/{category}/{key}/distribution', 'verb' => 'GET'],
-        ['name' => 'statistics#history', 'url' => '/api/v1/admin/{application}/metrics/{category}/{key}/history', 'verb' => 'GET'],
+        ['name' => 'statistics#numerical', 'url' => '/api/v1/admin/{application}/metrics/{category}/{key}/numerical', 'verb' => 'GET'],
+        ['name' => 'statistics#numericalHistory', 'url' => '/api/v1/admin/{application}/metrics/{category}/{key}/numerical/history', 'verb' => 'GET'],
     ],
 ];
