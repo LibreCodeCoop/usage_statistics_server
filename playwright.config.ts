17956571cb28a8c9b20860f98c1b56b089388cae
Vitor Mattos <vitor@php.rio>
2026-09-08 15:50:04 -0300

    test: serialize Playwright against CI server

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,8 @@ import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
 	testDir: './tests/playwright',
-	fullyParallel: true,
+	fullyParallel: false,
+	workers: process.env.CI ? 1 : undefined,
 	forbidOnly: Boolean(process.env.CI),
 	retries: process.env.CI ? 2 : 0,
 	reporter: process.env.CI ? [['github'], ['html', { open: 'never' }]] : 'list',
