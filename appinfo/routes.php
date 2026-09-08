eea554a4970887aee3a6f24ae9ed0354b0c7a559
Vitor Mattos <vitor@php.rio>
2026-09-07 21:59:52 -0300

    feat: add application schema routes

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 return [
     'ocs' => [
         ['name' => 'report#create', 'url' => '/api/v1/reports', 'verb' => 'POST'],
+        ['name' => 'schema#create', 'url' => '/api/v1/admin/schemas', 'verb' => 'POST'],
+        ['name' => 'schema#get', 'url' => '/api/v1/admin/schemas/{application}/{schemaVersion}', 'verb' => 'GET'],
         ['name' => 'statistics#summary', 'url' => '/api/v1/admin/{application}', 'verb' => 'GET'],
         ['name' => 'statistics#distribution', 'url' => '/api/v1/admin/{application}/metrics/{category}/{key}/distribution', 'verb' => 'GET'],
         ['name' => 'statistics#numerical', 'url' => '/api/v1/admin/{application}/metrics/{category}/{key}/numerical', 'verb' => 'GET'],
