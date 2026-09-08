917ff458f55d487001e651ef6577056e4723553e
Vitor Mattos <vitor@php.rio>
2026-09-07 21:34:08 -0300

    feat: add administrative statistics routes

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -5,5 +5,8 @@ declare(strict_types=1);
 return [
     'ocs' => [
         ['name' => 'report#create', 'url' => '/api/v1/reports', 'verb' => 'POST'],
+        ['name' => 'statistics#summary', 'url' => '/api/v1/admin/{application}', 'verb' => 'GET'],
+        ['name' => 'statistics#distribution', 'url' => '/api/v1/admin/{application}/metrics/{category}/{key}/distribution', 'verb' => 'GET'],
+        ['name' => 'statistics#history', 'url' => '/api/v1/admin/{application}/metrics/{category}/{key}/history', 'verb' => 'GET'],
     ],
 ];
